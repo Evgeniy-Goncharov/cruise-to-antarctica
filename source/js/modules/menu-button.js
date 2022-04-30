5ec146c1b5052f87e70996cd1f70d6da9db6edf0
siverskiy <siverskiy00@mail.ru>
2022-04-30 14:37:17 +0300

    Исправляет написание стилей

--- source/js/modules/menu-button.js
+++ source/js/modules/menu-button.js
@@ -1,28 +1,29 @@
-const header = document.querySelector('.page-header');
-const menuButton = header.querySelector('.menu-button');
-const nav = header.querySelector('.main-nav');
+const menuButton = document.querySelector('[data-menu=menu-button]');
+const menuElements = document.querySelectorAll('[data-menu]');
 
 const initMenu = () => {
   let isMenuOpen = false;
 
-  header.classList.remove('no-js');
-  menuButton.classList.remove('no-js');
-  nav.classList.remove('no-js');
+  menuElements.forEach((element) => {
+    element.classList.remove('no-js');
+  });
 
   const openMenu = () => {
-    header.classList.add('menu-open');
-    menuButton.classList.add('menu-open');
+    menuElements.forEach((element) => {
+      element.classList.add('menu-open');
+    });
+
     menuButton.ariaLabel = 'Закрыть меню';
-    nav.classList.add('menu-open');
     isMenuOpen = true;
     document.addEventListener('click', closeMenu);
   };
 
   const closeMenu = () => {
-    header.classList.remove('menu-open');
-    menuButton.classList.remove('menu-open');
+    menuElements.forEach((element) => {
+      element.classList.remove('menu-open');
+    });
+
     menuButton.ariaLabel = 'Открыть меню';
-    nav.classList.remove('menu-open');
     isMenuOpen = false;
     document.removeEventListener('click', closeMenu);
   };
